910ec21139c59f58ffdcd13d5892aabfcff1272b
benjamin <b.james.neustadt@gmail.com>
2022-09-10 21:02:22 +0200

    Refactor: use attributes
    * Public and Private sections give us better readability
    * attributes give us better error messages

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,13 +1,19 @@
 class Transaction
 
+  private
+
+  attr_writer :balance
+
   def initialize(balance: 0)
     @balance = balance
   end
 
+  public
+
   attr_reader :balance
 
   def deposit(amount)
-    @balance += amount
+    self.balance += amount
   end
 
 end
