37244812b7f42dbb0231ec8c9d6d70dd1660a93f
benjamin <b.james.neustadt@gmail.com>
2022-09-11 03:42:34 +0200

    Iterate through transaction history
    * Report uses the instance of Transactions history
      - It uses `format_events` to iterate over the history and then
        joins the collection of lines to a string

--- lib/report.rb
+++ lib/report.rb
@@ -1,7 +1,24 @@
+require_relative 'transaction'
+
 class Report
 
+  def initialize(transaction)
+    @transaction = transaction
+  end
+
+  attr_reader :transaction
+
+  def format_events
+    transaction.history
+      .map { |event|
+        "#{event[:date]} || #{event[:credit]} || #{event[:debit]}|| #{event[:balance]}\n"
+    }
+  end
+
   def report
-    'date || credit || debit || balance\n'
+    "date || credit || debit || balance\n" << format_events.first.to_s
   end
 
 end
+
+__END__
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -5,11 +5,19 @@ RSpec.describe 'Report' do
   describe 'report' do
 
     it 'initially returns a header' do
-      ledger = Report.new
-      expect(ledger.report).to eq 'date || credit || debit || balance\n'
+      ledger = Report.new(Transaction.new)
+      expect(ledger.report).to eq "date || credit || debit || balance\n"
+    end
+
+    it 'returns with a transaction event concatenated to the header' do
+      transaction = Transaction.new
+      transaction.deposit(3000)
+      date = Date.today.strftime('%d/%m/%Y')
+      ledger = Report.new(transaction)
+      expect(ledger.report)
+        .to eq "date || credit || debit || balance\n#{date} || 3000 || || 3000\n"
     end
 
   end
 
 end
-
